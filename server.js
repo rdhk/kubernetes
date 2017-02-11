4f7fd836d485ea3e472c0a5dca3c3cb0470ebeb4
Prashant Gupta <antshpra@gmail.com>
2017-02-11 16:36:43 +0530

    Updates and bugfixes in server logic

--- server.js
+++ server.js
@@ -17,11 +17,10 @@ const log = loggingClient.log('image-server');
 const metadata = { resource: { type: 'global' } };
 
 
-const cache = {};
+const fileMetaCache = {};
 
 
 const handleRequest = ( request, response ) => {
-
 
 	if( request.url == '/' ) {
 		response.writeHead(200);
@@ -49,71 +48,121 @@ const handleRequest = ( request, response ) => {
 	}
 
 
-
-	var fileName;
-
 	if( uri == '/author/image' ) {
 		
-		if( paramVals['authorId'] == null || paramVals[ 'version' ] == null ) {
+		if( paramVals[ 'version' ] == null ) {
 			dispatch404( response );
 			return;
 		}
 		
-		fileName = 'author-image-' + paramVals['authorId'] + '-' + paramVals[ 'version' ];
-		if( paramVals['width'] != null )
-			fileName = fileName + '-' + paramVals[ 'width' ];
 		
-		var fileMeta = fileMetaCache.get( fileName );
-		if( fileMeta != null ) {
-			dispatchFile( fileName, fileMeta );
-			return;
-		}
-		
-		fileName = 'author-image-' + paramVals['authorId'] + '-' + paramVals[ 'version' ];
-		fileMeta = fileMetaCache.get( fileName );
-		if( fileMeta == null )
-		
-		if( fileMeta == null && paramVals['width'] != null ) {
-			var originalFileName = 'author-image-' + paramVals['authorId'] + '-' + paramVals[ 'version' ];
+		if( paramVals['authorId'] == null )
+			dispatch( 'author/default/images/profile',
+					'/home/author-image-default',
+					paramVals[ 'width' ] == null ? null : paramVals[ 'width' ] + 'x' + paramVals[ 'width' ],
+					response );
+		else
+			dispatch( 'author/' + paramVals['authorId'] + '/images/profile/' + paramVals[ 'version' ],
+					'/home/author-image-' + paramVals['authorId'] + '-' + paramVals[ 'version' ],
+					paramVals[ 'width' ] == null ? null : paramVals[ 'width' ] + 'x' + paramVals[ 'width' ],
+					response );
 			
-		}
-				
-	}
-	
-	if( ! fs.existsSync( '/home/' + fileName + '.jpg' ) ) {
-		var file = bucket.file( 'author/' + paramVals['authorId'] + '/images/profile/' + paramVals[ 'version' ] );
-		console.log( 'Downloading ' + 'author/' + paramVals['authorId'] + '/images/profile/' + paramVals[ 'version' ] + ' ...' );
-		file.download( { destination: '/home/' + fileName + '.jpg' }, function( err ) {
-			console.log(err);
-			console.log( 'Download finished ...' );
-			
-			dispatch( '/home/' + fileName, paramVals['width'], response );
-		});
 	} else {
-			dispatch( '/home/' + fileName, paramVals['width'], response );
-	}
-	
 		
+		dispatch404( response );
+		
+	}
+
 };
 
-function dispatch( fileName, width, response ) {
 
-	if( width != null ) {
-		var resize = width + 'x' + width;
-		if( ! fs.existsSync( fileName + '-' + resize + '.jpg' ) )
-			execSync( 'convert ' + fileName + '.jpg -resize ' + resize + '! -unsharp 0x0.55+0.55+0.008 -quality 50% ' + fileName + '-' + resize + '.jpg' );
-		fileName = fileName + '-' + resize;
+
+function dispatch( gcsFileName, fileName, resize, response ) {
+	
+	if( ( resize != null && fileMetaCache[ fileName + '-' + resize ] != null )
+			|| ( resize == null && fileMetaCache[ fileName ] != null ) ) {
+
+		var fileMeta = resize == null
+				? fileMetaCache[ fileName ]
+				: fileMetaCache[ fileName + '-' + resize ];
+		
+		fileName = resize == null
+				? fileName + fileMeta[ 'ext' ]
+				: fileName + '-' + resize + fileMeta[ 'ext' ];
+		
+		var img = fs.readFileSync( fileName );
+		response.writeHead( 200, {'Content-Type': fileMeta[ 'type' ] } );
+		response.end( img, 'binary' );
+		
+	} else if( resize != null && fileMetaCache[ fileName ] != null ) {
+		
+		console.log( 'Resizing ' + fileName + ' ...' );
+		
+		var fileType = fileMetaCache[ fileName ][ 'type' ];
+		var fileExt = getFileExt( fileType );
+		execSync( 'convert '
+				+ fileName + fileExt + ' '
+				+ '-resize ' + resize + '! '
+				+ '-unsharp 0x0.55+0.55+0.008 '
+				+ '-quality 50% '
+				+ fileName + '-' + resize + fileExt );
+		
+		fileMetaCache[ fileName + '-' + resize ] = {
+			'ext': fileExt,
+			'type': fileType,
+			'lastAccessed': new Date()
+		};
+		
+		dispatch( gcsFileName, fileName, resize, response );
+		
+	} else if( fileMetaCache[ fileName ] == null ) {
+
+		console.log( 'Downloading ' + gcsFileName + ' ...' );
+		
+		var file = bucket.file( gcsFileName );
+		
+		file.getMetadata( function( err, metadata, apiResponse ) {
+			var fileType = metadata[ 'contentType' ];
+			if( fileType == null ) {
+				console.log( 'ContentType not set for ' + gcsFileName );
+				fileType = 'image/jpeg';
+			}
+			var fileExt = getFileExt( fileType );
+			file.download( { destination: fileName + fileExt }, function( err ) {
+				if( err == null ) {
+					// TODO: Compress image
+					fileMetaCache[ fileName ] = {
+						'ext': fileExt,
+						'type': fileType,
+						'lastAccessed': new Date()
+					};
+					dispatch( gcsFileName, fileName, resize, response );
+				} else {
+					console.log( err );
+					// TODO: Dispatch error
+				}
+			});
+		});
+		
 	}
 	
-	var img = fs.readFileSync( fileName + '.jpg' );
-//	response.writeHead( 200, {'Content-Type': 'image/png' } );
-	response.writeHead( 200 );
-	response.end( img, 'binary' );
-	
+}
+
+function getFileExt( fileType ) {
+	if( fileType.toLowerCase() == 'image/jpeg' )
+		return '.jpg';
+	else if( fileType.toLowerCase() == 'image/png' )
+		return '.png';
+	else {
+		console.log( 'Unsupported file type: ' + fileType );
+		return '.jpg';
+	}
 }
 
 function dispatch404( response ) {
-	
+	response.writeHead( 404 );
+	response.end( 'Image not found !' );
 }
+
 
 http.createServer( handleRequest ).listen(8080);
--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@ export CLUSTER="image-service"
 export SERVICE="image-service"
 export ZONE="asia-east1-a"
 
-kubectl set image deployment/$SERVICE-$1 $SERVICE-$1=gcr.io/$PROJECT_ID/$SERVICE:$2
+kubectl set image deployment/$SERVICE-$1 $SERVICE-$1=asia.gcr.io/$PROJECT_ID/$SERVICE:$2
 
 kubectl get pod
 kubectl get services $SERVICE-$1
