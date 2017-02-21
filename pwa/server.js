17cacd879f27082049fd183f2952084ea3537ea2
Prashant <antshpra@gmail.com>
2017-02-21 20:31:57 +0530

    [PWA] Bug fix in server logic

--- pwa/server.js
+++ pwa/server.js
@@ -6,7 +6,7 @@ const handleRequest = ( request, response ) => {
 	var url = request.url;
 	var uri = url.indexOf( '?' ) == -1 ? url : url.substring( 0, url.indexOf( '?' ) );
 	
-	var file = fs.readFileSync( fileName );
+	var file = fs.readFileSync( uri );
 	response.writeHead( 200, { 'Cache-Control': 'max-age=315360000, public' } ); // 10 Years
 	response.end( file, 'binary' );
 
