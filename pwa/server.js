b44efac648c7013dfb121a71ea62574f70f34d49
Prashant <antshpra@gmail.com>
2017-02-21 20:49:48 +0530

    [PWA] Redirecting to www.pratilipi.com for non-existent files.

--- pwa/server.js
+++ pwa/server.js
@@ -5,10 +5,16 @@ const handleRequest = ( request, response ) => {
 
 	var url = request.url;
 	var uri = url.indexOf( '?' ) == -1 ? url : url.substring( 0, url.indexOf( '?' ) );
+
+	if( fs.existsSync( uri ) ) {
+		var file = fs.readFileSync( uri );
+		response.writeHead( 200, { 'Cache-Control': 'max-age=315360000, public' } ); // 10 Years
+		response.end( file, 'binary' );
+	} else {
+		response.writeHead( 302, { 'Location': url.replace( 'pwa.', 'www.' ) } );
+		response.end();
+	}
 	
-	var file = fs.readFileSync( uri );
-	response.writeHead( 200, { 'Cache-Control': 'max-age=315360000, public' } ); // 10 Years
-	response.end( file, 'binary' );
 
 };
 
